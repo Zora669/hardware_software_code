18f62e9ab7f0f27970a5719cedf2553fba36feb0
Zora669 <izanieves1208@gmail.com>
2024-01-10 11:19:55 -0500

    1/10/24

---
+++ Your_Izaiah.py
@@ -0,0 +1,10 @@
+def main():
+    name = input("what is your name?")
+    print ("We want to know if you like programming!")
+    answer = input ("Do you like programming" + name + "?")
+    print ('Great! you said"' + answer + '"?"')
+    print ("let's learn some python today")
+
+
+if __name__== "__main__":
+    main()
---
+++ Zora_code.py
@@ -0,0 +1,11 @@
+def main ():
+    name = "Izaiah"
+    print("We want to know if you like programming!")
+    print()
+    print("do you like programming "+ name + "?")
+    answer = input()
+    print("Great! You said " + answer + "?")
+    print("let's start learning some Python today")
+
+if __name__ == "__main__":
+    main()
